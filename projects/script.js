ff3930b392261a77e97689dd3a3a5a948949e04b
Bhanteja072 <bhanutejapusarla@gmail.com>
2026-03-12 18:10:00 +0530

    Update portfolio

--- projects/script.js
+++ projects/script.js
@@ -45,9 +45,9 @@ function showProjects(projects) {
     let projectsHTML = "";
     projects.forEach(project => {
         projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+                <div class="grid-item ${project.category}">
+                <div class="box tilt">
+            <img draggable="false" src="../assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
